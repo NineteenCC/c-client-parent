edfe6bcbf228efaf5c0fdd717c460155525ff615
NineteenCC <95668230+NineteenCC@users.noreply.github.com>
2022-12-06 17:37:20 +0800

    Initial commit

---
+++ src/main/java/com/c/cclientparent/CClientParentApplication.java
@@ -0,0 +1,13 @@
+package com.c.cclientparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CClientParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CClientParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/c/cclientparent/CClientParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c.cclientparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CClientParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
