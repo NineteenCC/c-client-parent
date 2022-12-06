87d53150e5ae9c1d7ab9e5470e3b77c36b974e79
NineteenCC <95668230+NineteenCC@users.noreply.github.com>
2022-12-06 17:41:06 +0800

    init client

--- src/main/java/com/c/cclientparent/CClientParentApplication.java
+++ src/main/java/com/c/cclientparent/CClientParentApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CClientParentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CClientParentApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CClientParentApplication.class, args);
+    }
 
 }
